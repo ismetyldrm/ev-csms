4a2eac720b335f3f32240a5753d06b91ce9102f6
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-12 15:46:23 +0300

    Vehicle controller eklendi

---
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/api/controller/VehicleController.java
@@ -0,0 +1,45 @@
+package com.evcsms.chargestationserver.api.controller;
+
+import com.evcsms.chargestationserver.dto.CreateVehicleDTO;
+import com.evcsms.chargestationserver.service.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/vehicles")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Validated @RequestBody CreateVehicleDTO dto) {
+        Long id = vehicleService.createVehicle(dto);
+        URI location = URI.create("/api/v1/vehicles/" + id);
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CreateVehicleDTO>> getAll(Pageable pageable) {
+        Page<CreateVehicleDTO> vehicles = vehicleService.getAllVehicles(pageable);
+        return ResponseEntity.ok(vehicles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CreateVehicleDTO> getById(@PathVariable Long id) {
+        CreateVehicleDTO dto = vehicleService.getVehicleById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        vehicleService.deleteVehicle(id);
+        return ResponseEntity.ok().build();
+    }
+}
