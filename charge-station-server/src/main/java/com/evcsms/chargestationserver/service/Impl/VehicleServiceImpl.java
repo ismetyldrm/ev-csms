07474470b02fba6ea4e152051b43dd764ac8f834
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-14 15:12:16 +0300

    Adding updateVehicle method to VehicleServiceImpl

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/service/Impl/VehicleServiceImpl.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/service/Impl/VehicleServiceImpl.java
@@ -1,16 +1,22 @@
 package com.evcsms.chargestationserver.service.Impl;
 
 
+import com.evcsms.chargestationserver.dto.CreateChargeStationDTO;
 import com.evcsms.chargestationserver.dto.CreateVehicleDTO;
+import com.evcsms.chargestationserver.dto.UpdateChargeStationDTO;
+import com.evcsms.chargestationserver.dto.UpdateVehicleDTO;
 import com.evcsms.chargestationserver.mapper.VehicleMapper;
 import com.evcsms.chargestationserver.model.Vehicle;
 import com.evcsms.chargestationserver.repository.VehicleRepository;
 import com.evcsms.chargestationserver.service.VehicleService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +30,15 @@ public class VehicleServiceImpl implements VehicleService {
         Vehicle vehicle = vehicleMapper.toVehicle(dto);
         Vehicle newVehicle = vehicleRepository.save(vehicle);
         return newVehicle.getId();
+    }
+
+    @Transactional
+    public Vehicle updateVehicle(Long id, UpdateVehicleDTO dto) {
+        Vehicle vehicle = vehicleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Vehicle with id " + id + " does not exist"));
+        vehicleMapper.toVehicle(vehicle, dto);
+        return vehicleRepository.save(vehicle);
     }
 
     public Page<CreateVehicleDTO> getAllVehicles(Pageable pageable){
@@ -42,7 +57,4 @@ public class VehicleServiceImpl implements VehicleService {
                 .orElseThrow(() -> new IllegalArgumentException("Vehicle with id " + id + " does not exist"));
         vehicleRepository.delete(vehicle);
     }
-
-
-
 }
