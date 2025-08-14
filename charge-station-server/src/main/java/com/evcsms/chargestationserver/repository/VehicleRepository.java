f87be13c957ea386abb81c70a85508beaff93cf5
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-14 15:09:09 +0300

    Add findById method to VehicleRepository

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/VehicleRepository.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/VehicleRepository.java
@@ -3,7 +3,9 @@ package com.evcsms.chargestationserver.repository;
 import com.evcsms.chargestationserver.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
-
+    Optional<Vehicle> findById(Long id);
 }
