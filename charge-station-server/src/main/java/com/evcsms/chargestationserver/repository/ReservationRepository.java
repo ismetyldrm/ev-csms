9a9f2593ad262edddbe0ade9abe6107c496d4f68
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-14 15:08:21 +0300

    Minor formatting fix

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/ReservationRepository.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/ReservationRepository.java
@@ -4,4 +4,6 @@ import com.evcsms.chargestationserver.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+
 }
