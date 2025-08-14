868f857c0c35f6afe7bd2d8aad146c5f35f1a4d5
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-14 15:11:25 +0300

    Enhance ChargeStationServiceImpl update and create methods to manage charge points and connectors hierarchically, including add, update, and removal logic

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/service/Impl/ChargeStationServiceImpl.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/service/Impl/ChargeStationServiceImpl.java
@@ -1,24 +1,25 @@
 package com.evcsms.chargestationserver.service.Impl;
 
-import com.evcsms.chargestationserver.dto.CreateChargeStationDTO;
-import com.evcsms.chargestationserver.dto.UpdateChargePointDTO;
-import com.evcsms.chargestationserver.dto.UpdateChargeStationDTO;
-import com.evcsms.chargestationserver.dto.UpdateConnectorDTO;
+import com.evcsms.chargestationserver.dto.*;
 import com.evcsms.chargestationserver.mapper.ChargeStationMapper;
 import com.evcsms.chargestationserver.model.ChargePoint;
 import com.evcsms.chargestationserver.model.ChargeStation;
 import com.evcsms.chargestationserver.model.Connector;
 import com.evcsms.chargestationserver.repository.ChargeStationRepository;
 import com.evcsms.chargestationserver.service.ChargeStationService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,40 +31,50 @@ public class ChargeStationServiceImpl implements ChargeStationService {
     @Transactional
     public Long createChargeStation(CreateChargeStationDTO dto) {
         ChargeStation chargeStation = chargeStationMapper.toChargeStation(dto);
+
+        for (CreateChargePointDTO chargePointDTO : dto.getChargePoints()) {
+            ChargePoint chargePoint = chargeStationMapper.toChargePointForCreate(chargePointDTO);
+
+            for (CreateConnectorDTO connectorDTO : chargePointDTO.getConnectors()) {
+                Connector connector = chargeStationMapper.toConnectorForCreate(connectorDTO);
+                chargePoint.addConnector(connector);
+            }
+
+            chargeStation.addChargePoint(chargePoint);
+        }
+
         ChargeStation newChargeStation = chargeRepository.save(chargeStation);
         return newChargeStation.getId();
     }
 
     @Transactional
-    public List<CreateChargeStationDTO> updateChargeStation(Long id, UpdateChargeStationDTO dto) {
-        ChargeStation chargeStation = chargeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Charge Station with id " + id + " does not exist"));
+    public CreateChargeStationDTO updateChargeStation(Long id, UpdateChargeStationDTO dto) {
+        ChargeStation chargeStation = chargeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("ChargeStation not found with ID: " + id));
 
         chargeStationMapper.toChargeStation(chargeStation, dto);
-        // TODO: Implement the logic to update charge points and connectors
 
-        Iterator<ChargePoint> chargePointIterator = chargeStation.getChargePoints().iterator();
-        while (chargePointIterator.hasNext()) {
-            ChargePoint chargePoint = chargePointIterator.next();
-            Optional.ofNullable(dto.findChargePoint(chargePoint.getOcppId()))
-                    .ifPresentOrElse(chargePointDTO -> {
-                                chargeStationMapper.toChargePointForUpdate(chargePoint, chargePointDTO);
+        for (UpdateChargePointDTO chargePointDTO : dto.getChargePoints()) {
+            ChargePoint chargePoint = chargeStation.findChargePoint(chargePointDTO.getOcppId()).orElse(new ChargePoint());
+            chargeStationMapper.toChargePointForUpdate(chargePoint, chargePointDTO);
+            chargeStation.addChargePoint(chargePoint);
 
-                                Iterator<Connector> connectorIterator = chargePoint.getConnectors().iterator();
-                                while (connectorIterator.hasNext()) {
-                                    Connector connector = connectorIterator.next();
-                                    Optional.ofNullable(chargePointDTO.findConnector(connector.getIndex()))
-                                            .ifPresentOrElse(connectorDTO -> {
-                                                chargeStationMapper.toConnectorForUpdate(connector, (UpdateConnectorDTO) connectorDTO);
-                                            }, connectorIterator::remove);
-                                }
-                            },
-                            chargePointIterator::remove);
+            for (UpdateConnectorDTO connectorDTO : chargePointDTO.getConnectors()) {
+                Connector connector = chargePoint.findConnector(connectorDTO.getIndex()).orElse(new Connector());
+                chargeStationMapper.toConnectorForUpdate(connector, connectorDTO);
+                chargePoint.addConnector(connector);
+            }
+
+            chargePoint.removeIfNotIn(chargePointDTO.getConnectors().stream().map(UpdateConnectorDTO::getIndex).collect(Collectors.toList()));
         }
-        chargeStation = chargeRepository.saveAndFlush(chargeStation);
-        return chargeStationMapper.toDTOList(chargeRepository.findAll());
+
+        chargeStation.removeIfNotIn(dto.getChargePoints().stream().map(UpdateChargePointDTO::getOcppId).collect(Collectors.toList()));
+
+        ChargeStation updatedChargeStation = chargeRepository.save(chargeStation);
+
+        return chargeStationMapper.toDTO(updatedChargeStation);
     }
 
-    public Page<CreateChargeStationDTO> getAllChargeStations(Pageable pageable){
+    public Page<CreateChargeStationDTO> getAllChargeStations(Pageable pageable) {
         Page<ChargeStation> chargeStations = chargeRepository.findAll(pageable);
         return chargeStations.map(chargeStationMapper::toDTO);
     }
@@ -74,7 +85,7 @@ public class ChargeStationServiceImpl implements ChargeStationService {
         return chargeStationMapper.toDTO(chargeStation);
     }
 
-    public void deleteChargeStation(Long id){
+    public void deleteChargeStation(Long id) {
         ChargeStation chargeStation = chargeRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Charge Station with id " + id + " does not exist"));
         chargeRepository.delete(chargeStation);
