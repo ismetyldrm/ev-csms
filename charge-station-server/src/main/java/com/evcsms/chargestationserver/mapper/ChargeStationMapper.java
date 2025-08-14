082aa1732c2a86bc467ee162df32f1f3267c3199
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-14 14:35:00 +0300

    Update ChargeStationMapper: add version, created, updated ignores and refine mappings for ChargeStation, ChargePoint, and Connector

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/mapper/ChargeStationMapper.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/mapper/ChargeStationMapper.java
@@ -10,61 +10,60 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
-import java.util.Set;
 
-@Mapper(
-        componentModel = "spring"
-)
+@Mapper(componentModel = "spring")
 public abstract class ChargeStationMapper {
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "chargePoints", ignore = true)
     public abstract ChargeStation toChargeStation(CreateChargeStationDTO dto);
 
-    @AfterMapping
-    public void setChargePoints(@MappingTarget ChargeStation chargeStation) {
-        if (chargeStation.getChargePoints() != null) {
-            for (ChargePoint chargePoint : chargeStation.getChargePoints()) {
-                chargePoint.setChargeStation(chargeStation);
-            }
-        }
-    }
-
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
     @Mapping(target = "lastConnected", ignore = true)
     @Mapping(target = "lastDisconnected", ignore = true)
     @Mapping(target = "lastHealthChecked", ignore = true)
     @Mapping(target = "online", ignore = true)
     @Mapping(target = "chargeStation", ignore = true)
+    @Mapping(target = "connectors", ignore = true)
     public abstract ChargePoint toChargePointForCreate(CreateChargePointDTO dto);
 
-    //public abstract Set<ChargePoint> toCreateChargeStation(List<CreateChargePointDTO> dto);
-
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "chargePoints", ignore = true)
-    public abstract void toChargeStation(@MappingTarget ChargeStation chargeStation, UpdateChargeStationDTO dto);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "lastConnected", ignore = true)
-    @Mapping(target = "lastDisconnected", ignore = true)
-    @Mapping(target = "lastHealthChecked", ignore = true)
-    @Mapping(target = "online", ignore = true)
-    @Mapping(target = "chargeStation", ignore = true)
-    public abstract void toChargePointForUpdate(@MappingTarget ChargePoint chargePoint, UpdateChargePointDTO dto);
-
-    @AfterMapping
-    public void setConnectors(@MappingTarget ChargePoint chargePoint) {
-        if (chargePoint.getConnectors() != null) {
-            for (Connector connector : chargePoint.getConnectors()) {
-                connector.setChargePoint(chargePoint);
-            }
-        }
-    }
-
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
     @Mapping(target = "chargePoint", ignore = true)
     public abstract Connector toConnectorForCreate(CreateConnectorDTO dto);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "chargePoints", ignore = true)
+    public abstract void toChargeStation(@MappingTarget ChargeStation chargeStation, UpdateChargeStationDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "lastConnected", ignore = true)
+    @Mapping(target = "lastDisconnected", ignore = true)
+    @Mapping(target = "lastHealthChecked", ignore = true)
+    @Mapping(target = "online", ignore = true)
+    @Mapping(target = "chargeStation", ignore = true)
+    @Mapping(target = "connectors", ignore = true)
+    public abstract void toChargePointForUpdate(@MappingTarget ChargePoint chargePoint, UpdateChargePointDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "status", ignore = true)
     @Mapping(target = "chargePoint", ignore = true)
     public abstract void toConnectorForUpdate(@MappingTarget Connector connector, UpdateConnectorDTO dto);
 
