367e996249fbe92281e0dd0a8766a539c9a18600
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-22 16:45:42 +0300

    add ChargeHardwareSpecRepository

---
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/ChargeHardwareSpecRepository.java
@@ -0,0 +1,8 @@
+package com.evcsms.chargestationserver.repository;
+
+import com.evcsms.chargestationserver.model.ChargeHardwareSpec;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChargeHardwareSpecRepository extends JpaRepository<ChargeHardwareSpec, Long> {
+
+}
