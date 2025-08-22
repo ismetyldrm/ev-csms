7d8f4e4304d52e301a5b6db37b6be671b8ee5d9a
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-22 16:47:05 +0300

    Add findByOcppId method to ChargePointRepository

--- charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/ChargePointRepository.java
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/repository/ChargePointRepository.java
@@ -3,6 +3,8 @@ package com.evcsms.chargestationserver.repository;
 import com.evcsms.chargestationserver.model.ChargePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChargePointRepository extends JpaRepository<ChargePoint, Long> {
+import java.util.Optional;
 
+public interface ChargePointRepository extends JpaRepository<ChargePoint, Long> {
+    Optional<ChargePoint> findByOcppId(String ocppId);
 }
