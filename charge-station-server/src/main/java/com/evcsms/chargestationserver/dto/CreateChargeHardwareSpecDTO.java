b8844b9542eba7f3d0ec67cf8e9bb744b6833772
ismet yildirim <ismetyildirim2003@gmail.com>
2025-08-22 16:34:34 +0300

    Add DTO for creating Charge Hardware Specification

---
+++ charge-station-server/src/main/java/com/evcsms/chargestationserver/dto/CreateChargeHardwareSpecDTO.java
@@ -0,0 +1,30 @@
+package com.evcsms.chargestationserver.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateChargeHardwareSpecDTO {
+
+    private String chargePointVendor;
+
+    private String chargePointModel;
+
+    private String chargePointSerialNumber;
+
+    private String chargeBoxSerialNumber;
+
+    private String firmwareVersion;
+
+    private String iccid;
+
+    private String imsi;
+
+    private String meterType;
+
+    private String meterSerialNumber;
+
+    private Long chargePointId;
+}
